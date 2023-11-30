1d1214529d309e0b6600b240c69e205cced60408
rodrigosr82 <152562903+rodrigosr82@users.noreply.github.com>
2023-11-30 23:45:32 +0000

    (feat):

---
+++ Sum.py
@@ -0,0 +1,18 @@
+def sum_two_number(
+        a: float, 
+        b:float = 5
+) -> float: 
+    """Sum two values.
+
+    Take two varabls and sum.
+
+    Args:
+        a (float): Firt number.
+        b (float, optional): second number. Defults to 5.
+
+    Returns:
+        float: Sum of both arguments
+    """
+    num = a + b
+
+    return num
