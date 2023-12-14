5185a8893932bc1071a66fe13bed3ff297207e02
rodrigosr82 <152562903+rodrigosr82@users.noreply.github.com>
2023-12-14 23:42:21 +0000

    [feat]: created download.data[]

---
+++ data/download.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import pandas as pd
+
+def downloaddata (ticker:str) -> pd.DataFrame:
+    
+    """Download data from yahoo fiances
+
+    Args:
+        ticker (str): the ticker of financial asset.
+
+    Returns:
+        pd.DataFrame: datafram, restrived from yahoo Finances.
+    """
+
+    data = yf.download(ticker)
+    
+    return data
+
+
